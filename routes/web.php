1b7864e3e87b2d839fb2fd7d245bcd917ada14e2
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-09-28 14:06:20 +0200

    make it better

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\PokemonController;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PokemonController ;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PokemonController::class,'index']);
+Route::get('/',[PokemonController::class,'index']);

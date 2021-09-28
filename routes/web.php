642fe08fe8bb0d0904403e7777944aba2c41f5a9
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-09-28 14:19:29 +0200

    make it better

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\PokemonController;
+use App\Http\Controllers\PokemonController ;
 /*
 |--------------------------------------------------------------------------
 | Web Routes

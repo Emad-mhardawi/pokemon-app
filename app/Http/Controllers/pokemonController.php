2ee090322057a07e341b07f07d2360ef45669716
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-09-28 13:59:08 +0200

    make it better

--- app/Http/Controllers/pokemonController.php
+++ app/Http/Controllers/pokemonController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Pokemon;
-use Illuminate\Support\Facades\Http;
 
 class PokemonController extends Controller
 {
-    public function  index(){
-        return view('home');
+    public function index(){
+return view('home');
     }
+    
 }
